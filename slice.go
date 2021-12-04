9ed1bc4ad1f5a951a7091ce99db6a6562e644487
Wilfridovich17 <wilfridojpg@gmail.com>
2021-12-04 10:40:57 -0600

    Avance slice

--- slice.go
+++ slice.go
@@ -1,13 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
 
 func main() {
-	//user_numbers := make([]int, 3)
-	user_input := "A"
-	for user_input != "X" {
+	var counter, aux int
+	user_numbers := make([]int, 3)
+	user_input := "a"
+	for user_input != "x" {
 		fmt.Printf("Introduce a number: ")
 		fmt.Scan(&user_input)
-		fmt.Printf(user_input)
+		user_input = strings.ToLower(user_input)
+		if user_input != "x" && counter > 2 {
+			user_numbers = append(user_numbers, strconv.Atoi(user_input))
+		}
+		if user_input != "x" && counter <= 2 {
+			user_numbers[counter] = strconv.Atoi(user_input)
+		}
+		
+	}
+	for _, user_number := range user_numbers{
+		fmt.Println(user_number)
 	}
 }
