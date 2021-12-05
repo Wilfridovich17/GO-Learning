417cb4da2357c4fe52bc2975d0f46b8710f7d5e3
wparedes17 <95299096+wparedes17@users.noreply.github.com>
2021-12-04 21:46:52 -0600

    Update slice.go
    FInal version

--- slice.go
+++ slice.go
@@ -2,27 +2,45 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	"strconv"
+	"strings"
 )
 
+func sort(slice []int, new_number int) int {
+	for idx, value := range slice {
+		if value > new_number {
+			return (idx)
+		}
+	}
+	return (-1)
+}
+
 func main() {
-	var counter, aux int
+	var aux, counter, cut int
+	var err error
+	var user_input string = "a"
+
 	user_numbers := make([]int, 3)
-	user_input := "a"
+
 	for user_input != "x" {
 		fmt.Printf("Introduce a number: ")
 		fmt.Scan(&user_input)
-		user_input = strings.ToLower(user_input)
-		if user_input != "x" && counter > 2 {
-			user_numbers = append(user_numbers, strconv.Atoi(user_input))
+		aux, err = strconv.Atoi(user_input)
+		if err == nil {
+			switch {
+			case counter == 0:
+				user_numbers[counter] = aux
+			case counter > 0:
+				cut = sort(user_numbers, aux)
+				user_numbers = append(user_numbers[0:cut+1], user_numbers[cut:counter]...)
+				user_numbers[cut] = aux
+			}
+			counter++
+		} else {
+			user_input = strings.ToLower(user_input)
 		}
-		if user_input != "x" && counter <= 2 {
-			user_numbers[counter] = strconv.Atoi(user_input)
-		}
-		
 	}
-	for _, user_number := range user_numbers{
+	for _, user_number := range user_numbers {
 		fmt.Println(user_number)
 	}
 }
