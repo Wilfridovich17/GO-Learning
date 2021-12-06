dec8825e3925857599207d2644deb52c21c5249d
Wilfridovich17 <wilfridojpg@gmail.com>
2021-12-06 14:59:09 -0600

    makejson fix

--- makejson.go
+++ makejson.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
-	"os"
 	"encoding/json"
+	"fmt"
+	"os"
 )
 
-func main(){
+func main() {
 	my_map := make(map[string]string)
 
-	reader:= bufio.NewScanner(os.Stdin)
-
+	reader := bufio.NewScanner(os.Stdin)
 
 	fmt.Printf("Write your name: ")
 	reader.Scan()
 	my_map["name"] = reader.Text()
-	
+
 	fmt.Printf("Write your address: ")
 	reader.Scan()
 	my_map["address"] = reader.Text()
 
 	byte_rep, _ := json.Marshal(my_map)
-	
+
 	fmt.Printf("This is the JSON []byte representation:\n")
 	fmt.Println(byte_rep)
-
+	fmt.Printf("This is the JSON string representation:\n")
+	fmt.Println(string(byte_rep))
 }
