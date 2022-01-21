e4c5642c47005483a4e0c1674fe97e4ba477e338
Wilfridovich17 <wilfridojpg@gmail.com>
2022-01-21 17:14:02 -0600

    Printing digits

--- gitcol.go
+++ gitcol.go
@@ -4,7 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Github Collaboration")
+	fmt.Println("Printing digits")
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
+	}
+	fmt.Println("Printing even digits")
+	for i := 0; i < 10; i++ {
+		if i%2 == 1 {
+			fmt.Println(i)
+		}
 	}
 }
